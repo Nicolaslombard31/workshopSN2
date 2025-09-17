5c24d0f6f019a0f7ebacd6f0e87b57ddd1c860a5
Dxctor-_Sl3ep <m.demeuleneire@ecoles-epsi.net>
2025-09-17 22:36:36 +0200

    finish
    just chat accessible depuis url:
    http://127.0.0.1:8000/login/
    python manage.py createsuperuser pour crée agent nom -> mail -> mdp

--- DjangoProject/communication_secrete/urls.py
+++ DjangoProject/communication_secrete/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.chat_view, name="index"),  # la racine = chat
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     path("chat/", views.chat_view, name="chat"),
