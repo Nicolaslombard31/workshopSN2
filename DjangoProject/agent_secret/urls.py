936be59ee74863818ef383e25748825c252cc80b
Dxctor-_Sl3ep <m.demeuleneire@ecoles-epsi.net>
2025-09-17 22:16:01 +0200

    page login fonctionnel
    http://127.0.0.1:8000/login/

--- DjangoProject/agent_secret/urls.py
+++ DjangoProject/agent_secret/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("communication_secrete/", include("communication_secrete.urls")),
     path("admin/", admin.site.urls),
+    path("", include("communication_secrete.urls")),
 ]
