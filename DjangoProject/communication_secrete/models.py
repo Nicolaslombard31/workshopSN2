0e4a5182aa4130fa8f1dbb0784156a8d13bde6fd
Dxctor-_Sl3ep <m.demeuleneire@ecoles-epsi.net>
2025-09-17 22:02:45 +0200

    back up super user
    on passe à la page 3 de la doc

--- DjangoProject/communication_secrete/models.py
+++ DjangoProject/communication_secrete/models.py
@@ -1,14 +1,12 @@
-import datetime
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
 
-
 class Message(models.Model):
-    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sender', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='receiver', on_delete=models.CASCADE)
     message = models.TextField()
     date = models.DateTimeField(default=timezone.now)
     def __str__(self):
-        return f"{self.sender} → {self.receiver} : {self.message[:50]}  at {self.date}"
-
+        return f"{self.sender} → {self.receiver} : {self.message[:50]}  \n {self.date}"
