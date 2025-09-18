bf1729b52c9d13bfb77c2446ad14a591a338014a
Dxctor-_Sl3ep <m.demeuleneire@ecoles-epsi.net>
2025-09-18 09:58:26 +0200

    verification des infos privés

--- DjangoProject/agent_secret/settings.py
+++ DjangoProject/agent_secret/settings.py
@@ -1,4 +1,4 @@
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e*ln3gc687er^%ztc-9qn5bx_wc49mf&m+=d-f0#%1*4*1%11)'
+SECRET_KEY =  config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
